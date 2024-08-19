9aac39313862235ef49886fcbdd71c64f21a1705
heart14 <lwf14@qq.com>
2024-08-20 00:30:39 +0800

    feat:init repo and test run

---
+++ src/main/java/com/liiwe/WhatHappensTodayApplication.java
@@ -0,0 +1,11 @@
+package com.liiwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WhatHappensTodayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WhatHappensTodayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/liiwe/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.liiwe.controller;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wfli
+ * @since 2024/8/19 23:59
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public DemoController(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/{value}")
+    public String redis(@PathVariable("value") String value){
+        redisTemplate.opsForValue().setIfAbsent("test:key:"+value,value, 1000L,TimeUnit.SECONDS);
+        return "success:"+value;
+    }
+}
